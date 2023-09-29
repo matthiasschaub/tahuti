e0913c931c1870d525f2dd90af8897e911d20d2a
matthias_schaub <matthias.schaub@posteo.de>
2023-09-29 21:26:50 +0200

    test: refine API integration tests

--- tests/test_tahuti_api.py
+++ tests/test_tahuti_api.py
@@ -7,14 +7,13 @@ import uuid
 #
 group = Schema(
     {
-        "gid": str,
         "title": str,
         "host": str,
         "members": [str],
+        "acl": [str]
     },
 )
-groups = Schema([group])
-
+groups = Schema({str: group})
 
 uuid_ = str(uuid.uuid4())
 
@@ -26,6 +25,16 @@ def auth():
     with requests.Session() as session:
         response = session.post(url, data=data)  # perform the login
     return response.cookies
+
+
+@pytest.fixture(scope="session")
+def commit():
+    url = "http://localhost:12321"
+    data = {
+        "source": {"dojo": "+hood/commit %tahuti"},
+        "sink": {"app": "hood"},
+    }
+    requests.post(url, json=data)
 
 
 def test_redirect_unauthorized():
@@ -41,7 +50,7 @@ def test_root(auth):
     assert response.content == b"404 - Not Found"
 
 
-def test_foo(auth):
+def test_get_foo(auth):
     url = "http://localhost:8080/apps/tahuti/api/foo"
     response = requests.get(url, cookies=auth)
     assert response.status_code == 404
@@ -53,7 +62,7 @@ def test_get_groups(auth):
     response = requests.get(url, cookies=auth)
     assert response.status_code == 200
     result = response.json()
-    assert result == "zod"
+    assert groups.is_valid(result)
 
 
 def test_put_groups_empty(auth):
@@ -73,13 +82,34 @@ def test_put_groups_foo_empty(auth):
 @pytest.mark.parametrize(
     "uuid,given,expected",
     [
-        (uuid_, "foo", {uuid_: "foo"}),
-        (uuid_, "bar", {uuid_: "bar"}),
+        (
+            uuid_,
+            {"host": "~zod", "title": "foo", "members": [], "acl": []},
+            {uuid_: {"host": "~zod", "title": "foo", "members": [], "acl": []}},
+        ),
+        (
+            uuid_,
+            {"host": "~nus", "title": "bar", "members": ["~zod"], "acl": ["~zod"]},
+            {uuid_: {"host": "~nus", "title": "bar", "members": ["~zod"], "acl": ["~zod"]}},
+        ),
     ],
 )
 def test_put_groups(auth, uuid, given, expected):
     url = "http://localhost:8080/apps/tahuti/api/groups/" + uuid
     response = requests.put(url, cookies=auth, json=given)
     assert response.status_code == 200
+    url = "http://localhost:8080/apps/tahuti/api/groups"
+    response = requests.get(url, cookies=auth)
+    assert response.status_code == 200
     result = response.json()
-    assert result == expected
+    assert groups.is_valid(result)
+    # result = response.json()
+    # assert result == expected
+
+
+def test_put_expense(auth):
+    url = "http://localhost:8080/apps/tahuti/api/groups/foo/expenses/bar"
+    response = requests.put(url, cookies=auth, json={"amount": 3})
+    assert response.status_code == 200
+    result = response.json()
+    assert result == {"bar": {"amount": 3}}
