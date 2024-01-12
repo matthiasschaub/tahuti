805c5e8bb46bf22443f27daa654b8820a47791c2
matthias_schaub <matthias.schaub@posteo.de>
2024-01-12 23:24:24 +0100

    overhaul pwa ui

--- tahuti/app/ui/assets/request-expense.js
+++ tahuti/app/ui/assets/request-expense.js
@@ -1,41 +1,41 @@
 // extract group ID from URL
 //
 function gid() {
-    const parts = window.location.href.split("/");
-    const eid = parts.pop() || parts.pop(); // handle potential trailing slash
-    parts.pop() || parts.pop();
-    return parts.pop() || parts.pop();
+  const parts = window.location.href.split("/");
+  const eid = parts.pop() || parts.pop(); // handle potential trailing slash
+  parts.pop() || parts.pop();
+  return parts.pop() || parts.pop();
 }
 
 // extract expense ID from URL
 //
 function eid() {
-    const parts = window.location.href.split("/");
-    return parts.pop() || parts.pop(); // handle potential trailing slash
+  const parts = window.location.href.split("/");
+  return parts.pop() || parts.pop(); // handle potential trailing slash
 }
 
 // change HTMX request URL to make requests to the API
 //
 document.body.addEventListener("htmx:configRequest", (event) => {
-    if (event.detail.elt.id === "expense") {
-        // request expense
-        site = `/apps/tahuti/api/groups/${gid()}/expenses/${eid()}${
-            event.detail.path
-        }`;
-    } else {
-        // request group
-        site = `/apps/tahuti/api/groups/${gid()}${event.detail.path}`;
-    }
-    event.detail.path = site.replace(/\/$/, ""); // without trailing slash
+  if (event.detail.elt.id === "expense") {
+    // request expense
+    site = `/apps/tahuti/api/groups/${gid()}/expenses/${eid()}${
+      event.detail.path
+    }`;
+  } else {
+    // request group
+    site = `/apps/tahuti/api/groups/${gid()}${event.detail.path}`;
+  }
+  event.detail.path = site.replace(/\/$/, ""); // without trailing slash
 });
 
 // set HTML href URLs
 //
 const endpoints = ["add", "expenses", "balances", "members", "settings"];
 for (let i = 0; i < endpoints.length; i++) {
-    const element = document.getElementById(`${endpoints[i]}-href`);
-    if (element != null) {
-        const href = `/apps/tahuti/groups/${gid()}/${endpoints[i]}`;
-        element.setAttribute("href", href);
-    }
+  const element = document.getElementById(`${endpoints[i]}-href`);
+  if (element != null) {
+    const href = `/apps/tahuti/groups/${gid()}/${endpoints[i]}`;
+    element.setAttribute("href", href);
+  }
 }
--- tahuti/app/ui/assets/request-group.js
+++ tahuti/app/ui/assets/request-group.js
@@ -1,36 +1,36 @@
 // extract group ID from URL
 //
 function gid() {
-    const parts = window.location.href.split("/");
-    parts.pop() || parts.pop(); // handle potential trailing slash
-    return parts.pop() || parts.pop();
+  const parts = window.location.href.split("/");
+  parts.pop() || parts.pop(); // handle potential trailing slash
+  return parts.pop() || parts.pop();
 }
 
 // change HTMX request URL to make requests to the API
 //
 document.body.addEventListener("htmx:configRequest", (event) => {
-    site = `/apps/tahuti/api/groups/${gid()}${event.detail.path}`;
-    event.detail.path = site.replace(/\/$/, ""); // without trailing slash
+  site = `/apps/tahuti/api/groups/${gid()}${event.detail.path}`;
+  event.detail.path = site.replace(/\/$/, ""); // without trailing slash
 });
 
 // set HTML href and action URLs
 //
 const endpoints = [
-    "add",
-    "expenses",
-    "balances",
-    "members",
-    "settings",
-    "invite",
+  "add",
+  "expenses",
+  "balances",
+  "members",
+  "settings",
+  "invite",
 ];
 for (let i = 0; i < endpoints.length; i++) {
-    const element = document.getElementById(`${endpoints[i]}-href`);
-    if (element != null) {
-        const href = `/apps/tahuti/groups/${gid()}/${endpoints[i]}`;
-        element.setAttribute("href", href);
-    }
+  const element = document.getElementById(`${endpoints[i]}-href`);
+  if (element != null) {
+    const href = `/apps/tahuti/groups/${gid()}/${endpoints[i]}`;
+    element.setAttribute("href", href);
+  }
 }
 const btnInvite = document.getElementById("btn-invite");
 if (btnInvite != null) {
-    btnInvite.action = `/apps/tahuti/groups/${gid()}/invite`;
+  btnInvite.action = `/apps/tahuti/groups/${gid()}/invite`;
 }
