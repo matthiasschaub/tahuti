4ddbd0670548626aa1de4785b72b4db68b9d6c37
matthias_schaub <matthias.schaub@posteo.de>
2024-02-03 19:57:32 +0100

    fix(ui): fix delete button for an expense

--- ui/js/index.js
+++ ui/js/index.js
@@ -2,17 +2,28 @@ import 'htmx.org';
 import 'mustache';
 import * as currency from 'currency.js';
 
+// extract group ID from URL
+//
 function gid() {
   const parts = window.location.href.split("/");
   parts.pop() || parts.pop(); // handle potential trailing slash
   return parts.pop() || parts.pop();
 };
 
+// extract expense ID from URL
+//
+function eid() {
+  const parts = window.location.href.split("/");
+  return parts.pop() || parts.pop(); // handle potential trailing slash
+}
+
 export {
     gid,
+    eid,
 };
 
 window.htmx = require('htmx.org');
 window.Mustache = require('mustache');
 window.currency = require('currency.js');
 window.gid = gid;
+window.eid = eid;
