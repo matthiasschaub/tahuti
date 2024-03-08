e36f1c2ffaef4e7a61136b5539b22d5adf991be1
matthias_schaub <matthias.schaub@posteo.de>
2024-03-08 16:54:53 +0100

    build: move Python and NPM setup stuff to root

--- build-ui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-echo "Build UI"
-cleancss -o "tahuti/app/ui/static/css/min/style.css" "ui/css/style.css"
-cleancss -o "tahuti/app/ui/static/css/min/print.css" "ui/css/print.css"
-echo "OK"
